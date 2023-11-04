679d3fd69440825ab9e9ca558f72c7b2eab286a1
Lauro Frei <laurofrei@outlook.com>
2023-11-04 16:56:29 +0100

    fixed folder path and filename errors

--- std-proxy-cache/scripts/update-proxy-config.sh
+++ std-proxy-cache/scripts/update-proxy-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /root/scripts/lanparty/common.inc
+
 service nginx stop
 
 rm -rf /etc/nginx/conf.d
@@ -15,5 +17,14 @@ ln -sf /etc/nginx/stream-available/* /etc/nginx/stream-enabled/
 
 mkdir -p /etc/nginx/sites-enabled
 ln -sf /etc/nginx/sites-available/* /etc/nginx/sites-enabled/
+
+# Configure variables
+set -e
+sed -i "s/CACHE_INDEX_SIZE/${CACHE_INDEX_SIZE}/" /etc/nginx/conf.d/20_proxy_cache_path.conf
+sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/conf.d/20_proxy_cache_path.conf
+sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/" /etc/nginx/sites-available/cache.conf.d/root/20_cache.conf
+sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/" /etc/nginx/conf.d/20_proxy_cache_path.conf
+sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/" /etc/nginx/sites-available/cache.conf.d/10_root.conf
+sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/" /etc/nginx/stream-available/10_sni.conf.conf
 
 service nginx start
