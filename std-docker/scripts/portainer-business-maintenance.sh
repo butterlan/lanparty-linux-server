134abd5fcfb41eae71bcc6caafa830200848f035
Lauro Frei <laurofrei@outlook.com>
2023-11-05 12:39:42 +0100

    fixed tempate path

--- std-docker/scripts/portainer-business-maintenance.sh
+++ std-docker/scripts/portainer-business-maintenance.sh
@@ -7,4 +7,4 @@ docker rm portainer
 
 docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer \
     --restart=always --pull=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest \
-    --templates https://bitbucket.org/apetomate/portainer-templates/raw/master/templates.json
+    --templates https://raw.githubusercontent.com/butterlan/butterlan-portainer-templates/main/templates.json
--- std-docker/scripts/portainer-maintenance.sh
+++ std-docker/scripts/portainer-maintenance.sh
@@ -9,4 +9,4 @@ docker pull portainer/portainer-ce:latest
 
 docker run -d -p 9443:9443 -p 8000:8000 --name portainer \ 
     --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce \
-    --templates https://bitbucket.org/apetomate/portainer-templates/raw/master/templates.json
+    --templates https://raw.githubusercontent.com/butterlan/butterlan-portainer-templates/main/templates.json
