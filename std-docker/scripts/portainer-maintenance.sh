9dedaa8df86712bca551a2be26d721c92e6112f6
Lauro Frei <laurofrei@outlook.com>
2023-11-05 12:34:46 +0100

    error in linebreaks

--- std-docker/scripts/portainer-maintenance.sh
+++ std-docker/scripts/portainer-maintenance.sh
@@ -7,6 +7,6 @@ docker stop portainer
 docker rm portainer
 docker pull portainer/portainer-ce:latest
 
-docker run -d -p 9443:9443 -p 8000:8000 --name portainer --restart always \
-           -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce \
-           --templates https://bitbucket.org/apetomate/portainer-templates/raw/master/templates.json
+docker run -d -p 9443:9443 -p 8000:8000 --name portainer \ 
+    --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce \
+    --templates https://bitbucket.org/apetomate/portainer-templates/raw/master/templates.json
